bc8a0bb347d0fb5f6e1a4689163b448e8ae6ba6c
DESKTOP-FAU8QB2\Jeff <backbone57@gmail.com>
2017-01-08 22:30:37 +0100

    3 -58

--- BuildingEscape/Source/BuildingEscape/RockPosRep.cpp
+++ BuildingEscape/Source/BuildingEscape/RockPosRep.cpp
@@ -1,27 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "RockPosRep.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+URockPosRep::URockPosRep()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
-
-	
 
 	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void URockPosRep::BeginPlay()
 {
 	Super::BeginPlay();
+
 	FString ObjectName = GetOwner()->GetName();
 	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
 	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
@@ -30,7 +28,7 @@ void UPositionReport::BeginPlay()
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void URockPosRep::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
